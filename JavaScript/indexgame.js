83ed66c798e80eca21347b1ed02a1d8eec7b7c71
Kew707 <neuled19@htl-kaindorf.at>
2022-06-02 16:31:54 +0200

    finally!!!
    das hat soo lange gedauert, es sind einfach so viele fricking fehlermeldungen wegen dem scheiß import gekommen ich weine QwQ

--- JavaScript/indexgame.js
+++ JavaScript/indexgame.js
@@ -1,1 +1,28 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from './setup.js';
+
+import GameBoard from './GameBoard.js';
+
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
+
+const GLOBAL_SPEED = 80;
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+let gameWin = false;
+
+function gameOver(pacman, grid){
+
+}
+
+function checkCollision(pacman, ghosts){
+
+}
+
+function gameLoop(pacman, ghosts){
+
+}
+
+function startGame(){
+
+}
